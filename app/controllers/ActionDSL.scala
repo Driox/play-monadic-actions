778c7355081511c24c661eebdcb9a9a11e4be753
Adrien <adrien.crovetto@gmail.com>
2016-01-06 10:45:05 +0100

    handle Either

--- app/controllers/ActionDSL.scala
+++ app/controllers/ActionDSL.scala
@@ -13,68 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package controllers
-
+package api.controllers
 
 import play.api.data.Form
 import play.api.data.validation.ValidationError
 import play.api.libs.json.{JsPath, JsResult}
 import play.api.mvc.{Results, Result}
-import scalaz.syntax.either._
+
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
+import scalaz.syntax.either._
 import scalaz.syntax.std.option._
 import scalaz._
 
 /**
  * @author Valentin Kasas
  */
-package object ActionDSL {
+object ActionDSL {
 
   type Step[A] = EitherT[Future, Result, A]
   type JsErrorContent = Seq[(JsPath, Seq[ValidationError])]
 
-  private [ActionDSL] def fromFuture[A](onFailure: Throwable => Result)(future: Future[A])(implicit ec: ExecutionContext): Step[A] =
+  private[ActionDSL] def fromFuture[A](onFailure: Throwable => Result)(future: Future[A])(implicit ec: ExecutionContext): Step[A] =
     EitherT[Future, Result, A](
-      future.map(_.right).recover{
+      future.map(_.right).recover {
         case NonFatal(t) => onFailure(t).left
       }
     )
 
-  private [ActionDSL] def fromFOption[A](onNone: => Result)(fOption: Future[Option[A]])(implicit ec: ExecutionContext): Step[A] =
+  private[ActionDSL] def fromFOption[A](onNone: => Result)(fOption: Future[Option[A]])(implicit ec: ExecutionContext): Step[A] =
     EitherT[Future, Result, A](fOption.map(_ \/> onNone))
 
-  private [ActionDSL] def fromFEither[A,B](onLeft: B => Result)(fEither: Future[Either[B,A]])(implicit ec: ExecutionContext): Step[A] =
-    EitherT[Future, Result, A](fEither.map(_.fold(onLeft andThen \/.left,\/.right)))
+  private[ActionDSL] def fromFEither[A, B](onLeft: B => Result)(fEither: Future[Either[B, A]])(implicit ec: ExecutionContext): Step[A] =
+    EitherT[Future, Result, A](fEither.map(_.fold(onLeft andThen \/.left, \/.right)))
 
-  private [ActionDSL] def fromFDisjunction[A,B](onLeft: B => Result)(fDisjunction: Future[B \/ A])(implicit ec: ExecutionContext): Step[A] =
+  private[ActionDSL] def fromFDisjunction[A, B](onLeft: B => Result)(fDisjunction: Future[B \/ A])(implicit ec: ExecutionContext): Step[A] =
     EitherT[Future, Result, A](fDisjunction.map(_.leftMap(onLeft)))
 
-  private [ActionDSL] def fromOption[A](onNone: => Result)(option: Option[A]): Step[A] =
+  private[ActionDSL] def fromOption[A](onNone: => Result)(option: Option[A]): Step[A] =
     EitherT[Future, Result, A](Future.successful(option \/> onNone))
 
-  private [ActionDSL] def fromJsResult[A](onJsError: JsErrorContent => Result)(jsResult: JsResult[A]): Step[A] =
+  private[ActionDSL] def fromEither[A, B](onLeft: B => Result)(either: Either[B, A])(implicit ec: ExecutionContext): Step[A] =
+    EitherT[Future, Result, A](Future.successful(either.fold(onLeft andThen \/.left, \/.right)))
+
+  private[ActionDSL] def fromJsResult[A](onJsError: JsErrorContent => Result)(jsResult: JsResult[A]): Step[A] =
     EitherT[Future, Result, A](Future.successful(jsResult.fold(onJsError andThen \/.left, \/.right)))
 
-  private [ActionDSL] def fromForm[A](onError: Form[A] => Result)(form: Form[A]): Step[A] =
+  private[ActionDSL] def fromForm[A](onError: Form[A] => Result)(form: Form[A]): Step[A] =
     EitherT[Future, Result, A](Future.successful(form.fold(onError andThen \/.left, \/.right)))
 
-  private [ActionDSL] def fromBoolean(onFalse: => Result)(boolean: Boolean): Step[Unit] =
+  private[ActionDSL] def fromBoolean(onFalse: => Result)(boolean: Boolean): Step[Unit] =
     EitherT[Future, Result, Unit](Future.successful(if (boolean) ().right else onFalse.left))
 
-  private [ActionDSL] def fromTry[A](onFailure: Throwable => Result)(tryValue: Try[A]):Step[A] =
+  private[ActionDSL] def fromTry[A](onFailure: Throwable => Result)(tryValue: Try[A]): Step[A] =
     EitherT[Future, Result, A](Future.successful(tryValue match {
       case Failure(t) => onFailure(t).left
       case Success(v) => v.right
     }))
 
   trait StepOps[A, B] {
-    def orFailWith(failureHandler: B => Result):Step[A]
+    def orFailWith(failureHandler: B => Result): Step[A]
     def ?|(failureHandler: B => Result): Step[A] = orFailWith(failureHandler)
     def ?|(failureThunk: => Result): Step[A] = orFailWith(_ => failureThunk)
   }
-
 
   trait MonadicActions {
 
@@ -104,20 +106,24 @@ package object ActionDSL {
       override def orFailWith(failureHandler: (Throwable) => Result) = fromFuture(failureHandler)(future)(executionContext)
     }
 
-    implicit def fOptionToStepOps[A](fOption: Future[Option[A]]):StepOps[A,Unit] = new StepOps[A, Unit]{
+    implicit def fOptionToStepOps[A](fOption: Future[Option[A]]): StepOps[A, Unit] = new StepOps[A, Unit] {
       override def orFailWith(failureHandler: Unit => Result) = fromFOption(failureHandler(()))(fOption)(executionContext)
     }
 
-    implicit def fEitherToStepOps[A, B](fEither: Future[Either[B,A]]): StepOps[A,B] = new StepOps[A,B] {
+    implicit def fEitherToStepOps[A, B](fEither: Future[Either[B, A]]): StepOps[A, B] = new StepOps[A, B] {
       override def orFailWith(failureHandler: (B) => Result) = fromFEither(failureHandler)(fEither)(executionContext)
     }
 
-    implicit def fDisjunctionToStepOps[A, B](fDisjunction: Future[B \/ A]): StepOps[A,B] = new StepOps[A, B] {
+    implicit def fDisjunctionToStepOps[A, B](fDisjunction: Future[B \/ A]): StepOps[A, B] = new StepOps[A, B] {
       override def orFailWith(failureHandler: (B) => Result) = fromFDisjunction(failureHandler)(fDisjunction)(executionContext)
     }
 
-    implicit def optionToStepOps[A](option: Option[A]):StepOps[A, Unit] = new StepOps[A, Unit] {
+    implicit def optionToStepOps[A](option: Option[A]): StepOps[A, Unit] = new StepOps[A, Unit] {
       override def orFailWith(failureHandler: (Unit) => Result) = fromOption(failureHandler(()))(option)
+    }
+
+    implicit def eitherToStepOps[A, B](either: Either[B, A]): StepOps[A, B] = new StepOps[A, B] {
+      override def orFailWith(failureHandler: (B) => Result) = fromEither(failureHandler)(either)(executionContext)
     }
 
     implicit def jsResultToStepOps[A](jsResult: JsResult[A]): StepOps[A, JsErrorContent] = new StepOps[A, JsErrorContent] {
